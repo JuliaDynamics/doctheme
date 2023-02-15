4f2e3152b7ffafc0a3de48c1175cf7b3c516216a
Datseris <datseris.george@gmail.com>
2023-02-15 16:08:52 +0000

    fix typos and wrong links

--- apply_style.jl
+++ apply_style.jl
@@ -5,19 +5,19 @@ using DocumenterTools: Themes
 ENV["JULIA_DEBUG"] = "Documenter"
 # download the themes
 import Downloads
-for file in ("juliaclimate-lightdefs.scss", "juliaclimate-darkdefs.scss", "juliaclimate-style.scss")
-    Downloads.download("https://raw.githubusercontent.com/JuliaClimate/doctheme/master/$file", joinpath(@__DIR__, file))
+for file in ("juliadynamics-lightdefs.scss", "juliadynamics-darkdefs.scss", "juliadynamics-style.scss")
+    Downloads.download("https://raw.githubusercontent.com/JuliaDynamics/doctheme/master/$file", joinpath(@__DIR__, file))
 end
 # create the themes
 for w in ("light", "dark")
-    header = read(joinpath(@__DIR__, "juliaclimate-style.scss"), String)
-    theme = read(joinpath(@__DIR__, "juliaclimate-$(w)defs.scss"), String)
-    write(joinpath(@__DIR__, "juliaclimate-$(w).scss"), header*"\n"*theme)
+    header = read(joinpath(@__DIR__, "juliadynamics-style.scss"), String)
+    theme = read(joinpath(@__DIR__, "juliadynamics-$(w)defs.scss"), String)
+    write(joinpath(@__DIR__, "juliadynamics-$(w).scss"), header*"\n"*theme)
 end
 # compile the themes
-Themes.compile(joinpath(@__DIR__, "juliaclimate-light.scss"), joinpath(@__DIR__, "src/assets/themes/documenter-light.css"))
-Themes.compile(joinpath(@__DIR__, "juliaclimate-dark.scss"), joinpath(@__DIR__, "src/assets/themes/documenter-dark.css"))
+Themes.compile(joinpath(@__DIR__, "juliadynamics-light.scss"), joinpath(@__DIR__, "src/assets/themes/documenter-light.css"))
+Themes.compile(joinpath(@__DIR__, "juliadynamics-dark.scss"), joinpath(@__DIR__, "src/assets/themes/documenter-dark.css"))
 # Download and apply CairoMakie plotting style
 using CairoMakie
-Downloads.download("https://raw.githubusercontent.com/JuliaClimate/doctheme/master/style.jl", joinpath(@__DIR__, file))
+Downloads.download("https://raw.githubusercontent.com/JuliaDynamics/doctheme/master/style.jl", joinpath(@__DIR__, "style.jl"))
 include("style.jl")
