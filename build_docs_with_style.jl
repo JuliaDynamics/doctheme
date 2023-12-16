1d17210056956b6d01161f5fe6dbffcd401093bf
Kristian Agasøster Haaga <kahaaga@gmail.com>
2023-12-16 12:07:33 +0100

    Download from correct repo

--- build_docs_with_style.jl
+++ build_docs_with_style.jl
@@ -1,29 +1,36 @@
 CI = get(ENV, "CI", nothing) == "true" || get(ENV, "GITHUB_TOKEN", nothing) !== nothing
 
 import Pkg
-Pkg.pkg"add Documenter@1.2"
+Pkg.pkg"add Documenter@1"
 
 # Load documenter
 using Documenter
 using DocumenterTools: Themes
 ENV["JULIA_DEBUG"] = "Documenter"
-# download the themes
+
+# For easier debugging when downloading from a specific branch.
+github_user = "kahaaga"
+branch = "documenter_v1"
+
 import Downloads
 for file in ("juliadynamics-lightdefs.scss", "juliadynamics-darkdefs.scss", "juliadynamics-style.scss")
-    Downloads.download("https://raw.githubusercontent.com/kahaaga/doctheme/master/$file", joinpath(@__DIR__, file))
+    Downloads.download("https://raw.githubusercontent.com/$github_user/doctheme/$branch/$file", joinpath(@__DIR__, file))
 end
+
 # create the themes
 for w in ("light", "dark")
     header = read(joinpath(@__DIR__, "juliadynamics-style.scss"), String)
     theme = read(joinpath(@__DIR__, "juliadynamics-$(w)defs.scss"), String)
     write(joinpath(@__DIR__, "juliadynamics-$(w).scss"), header*"\n"*theme)
 end
+
 # compile the themes
 Themes.compile(joinpath(@__DIR__, "juliadynamics-light.scss"), joinpath(@__DIR__, "src/assets/themes/documenter-light.css"))
 Themes.compile(joinpath(@__DIR__, "juliadynamics-dark.scss"), joinpath(@__DIR__, "src/assets/themes/documenter-dark.css"))
+
 # Download and apply CairoMakie plotting style
 using CairoMakie
-Downloads.download("https://raw.githubusercontent.com/kahaaga/doctheme/master/style.jl", joinpath(@__DIR__, "style.jl"))
+Downloads.download("https://raw.githubusercontent.com/$github_user/doctheme/$branch/style.jl", joinpath(@__DIR__, "style.jl"))
 include("style.jl")
 
 function build_docs_with_style(pages, modules...; bib = nothing, authors = "George Datseris", draft = false, kwargs...)
